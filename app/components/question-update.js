0f8d77f8f08e08729b80b87cc748ab37a95110c5
Marty <tyedye105@Martys-MacBook-Pro.local>
2017-01-13 12:44:51 -0800

    add update component for questions

--- app/components/question-update.js
+++ app/components/question-update.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
+  actions: {
+    showUpdateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    updateQuestion(question) {
+      debugger;
+      var params ={
+        content: this.get('content'),
+        author: this.get('author'),
+        topic: this.get('topic'),
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('updateQuestion', question, params);
+    }
+  }
 });
