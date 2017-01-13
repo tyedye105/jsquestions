b9c235199c150e33cb119212d313c0ed1dfe5c41
Marty <tyedye105@Martys-MacBook-Pro.local>
2017-01-13 11:13:10 -0800

    add new-question component to index, pass action saveQuestion up to index.

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    saveQuestion() {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        topic: this.get('topic')
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params)
+    }
+  }
 });
