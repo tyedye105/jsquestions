3b8db4fb29ac26deb187990097f34d88477ef755
Marty <tyedye105@Martys-MacBook-Pro.local>
2017-01-13 14:40:59 -0800

    setup one to many relationship between questions and answers, create new answer component

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author:DS.attr(),
   content:DS.attr(),
-  topic:DS.attr()
+  topic:DS.attr(),
+  answers: DS.hasMany('answer', {async: true})
 });
